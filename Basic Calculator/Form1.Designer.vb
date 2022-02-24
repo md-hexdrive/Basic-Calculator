0d3931eea76e1b24af4cdb2b648519fcffe2794a
md-hexdrive <57779180+md-hexdrive@users.noreply.github.com>
2022-02-24 11:30:23 -0500

    Add project files.

---
+++ Basic Calculator/Form1.Designer.vb
@@ -0,0 +1,272 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.OutputTextBox = New System.Windows.Forms.TextBox()
+        Me.FourButton = New System.Windows.Forms.Button()
+        Me.FiveButton = New System.Windows.Forms.Button()
+        Me.SixButton = New System.Windows.Forms.Button()
+        Me.NineButton = New System.Windows.Forms.Button()
+        Me.EightButton = New System.Windows.Forms.Button()
+        Me.SevenButton = New System.Windows.Forms.Button()
+        Me.ThreeButton = New System.Windows.Forms.Button()
+        Me.TwoButton = New System.Windows.Forms.Button()
+        Me.OneButton = New System.Windows.Forms.Button()
+        Me.EqualsButton = New System.Windows.Forms.Button()
+        Me.ZeroButton = New System.Windows.Forms.Button()
+        Me.DivideButton = New System.Windows.Forms.Button()
+        Me.MultButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.MinusButton = New System.Windows.Forms.Button()
+        Me.DecimalPointButton = New System.Windows.Forms.Button()
+        Me.PlusButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'OutputTextBox
+        '
+        Me.OutputTextBox.Font = New System.Drawing.Font("Segoe UI", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.OutputTextBox.Location = New System.Drawing.Point(60, 45)
+        Me.OutputTextBox.Name = "OutputTextBox"
+        Me.OutputTextBox.Size = New System.Drawing.Size(259, 36)
+        Me.OutputTextBox.TabIndex = 0
+        '
+        'FourButton
+        '
+        Me.FourButton.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.FourButton.Location = New System.Drawing.Point(60, 213)
+        Me.FourButton.Name = "FourButton"
+        Me.FourButton.Size = New System.Drawing.Size(60, 57)
+        Me.FourButton.TabIndex = 1
+        Me.FourButton.Text = "4"
+        Me.FourButton.UseVisualStyleBackColor = True
+        '
+        'FiveButton
+        '
+        Me.FiveButton.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.FiveButton.Location = New System.Drawing.Point(126, 213)
+        Me.FiveButton.Name = "FiveButton"
+        Me.FiveButton.Size = New System.Drawing.Size(60, 57)
+        Me.FiveButton.TabIndex = 1
+        Me.FiveButton.Text = "5"
+        Me.FiveButton.UseVisualStyleBackColor = True
+        '
+        'SixButton
+        '
+        Me.SixButton.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.SixButton.Location = New System.Drawing.Point(192, 213)
+        Me.SixButton.Name = "SixButton"
+        Me.SixButton.Size = New System.Drawing.Size(60, 57)
+        Me.SixButton.TabIndex = 1
+        Me.SixButton.Text = "6"
+        Me.SixButton.UseVisualStyleBackColor = True
+        '
+        'NineButton
+        '
+        Me.NineButton.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.NineButton.Location = New System.Drawing.Point(192, 150)
+        Me.NineButton.Name = "NineButton"
+        Me.NineButton.Size = New System.Drawing.Size(60, 57)
+        Me.NineButton.TabIndex = 2
+        Me.NineButton.Text = "9"
+        Me.NineButton.UseVisualStyleBackColor = True
+        '
+        'EightButton
+        '
+        Me.EightButton.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.EightButton.Location = New System.Drawing.Point(126, 150)
+        Me.EightButton.Name = "EightButton"
+        Me.EightButton.Size = New System.Drawing.Size(60, 57)
+        Me.EightButton.TabIndex = 3
+        Me.EightButton.Text = "8"
+        Me.EightButton.UseVisualStyleBackColor = True
+        '
+        'SevenButton
+        '
+        Me.SevenButton.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.SevenButton.Location = New System.Drawing.Point(60, 150)
+        Me.SevenButton.Name = "SevenButton"
+        Me.SevenButton.Size = New System.Drawing.Size(60, 57)
+        Me.SevenButton.TabIndex = 4
+        Me.SevenButton.Text = "7"
+        Me.SevenButton.UseVisualStyleBackColor = True
+        '
+        'ThreeButton
+        '
+        Me.ThreeButton.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.ThreeButton.Location = New System.Drawing.Point(192, 276)
+        Me.ThreeButton.Name = "ThreeButton"
+        Me.ThreeButton.Size = New System.Drawing.Size(60, 57)
+        Me.ThreeButton.TabIndex = 5
+        Me.ThreeButton.Text = "3"
+        Me.ThreeButton.UseVisualStyleBackColor = True
+        '
+        'TwoButton
+        '
+        Me.TwoButton.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TwoButton.Location = New System.Drawing.Point(126, 276)
+        Me.TwoButton.Name = "TwoButton"
+        Me.TwoButton.Size = New System.Drawing.Size(60, 57)
+        Me.TwoButton.TabIndex = 6
+        Me.TwoButton.Text = "2"
+        Me.TwoButton.UseVisualStyleBackColor = True
+        '
+        'OneButton
+        '
+        Me.OneButton.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.OneButton.Location = New System.Drawing.Point(60, 276)
+        Me.OneButton.Name = "OneButton"
+        Me.OneButton.Size = New System.Drawing.Size(60, 57)
+        Me.OneButton.TabIndex = 7
+        Me.OneButton.Text = "1"
+        Me.OneButton.UseVisualStyleBackColor = True
+        '
+        'EqualsButton
+        '
+        Me.EqualsButton.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.EqualsButton.Location = New System.Drawing.Point(192, 339)
+        Me.EqualsButton.Name = "EqualsButton"
+        Me.EqualsButton.Size = New System.Drawing.Size(61, 57)
+        Me.EqualsButton.TabIndex = 5
+        Me.EqualsButton.Text = "="
+        Me.EqualsButton.UseVisualStyleBackColor = True
+        '
+        'ZeroButton
+        '
+        Me.ZeroButton.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.ZeroButton.Location = New System.Drawing.Point(60, 339)
+        Me.ZeroButton.Name = "ZeroButton"
+        Me.ZeroButton.Size = New System.Drawing.Size(60, 57)
+        Me.ZeroButton.TabIndex = 5
+        Me.ZeroButton.Text = "0"
+        Me.ZeroButton.UseVisualStyleBackColor = True
+        '
+        'DivideButton
+        '
+        Me.DivideButton.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.DivideButton.Location = New System.Drawing.Point(259, 339)
+        Me.DivideButton.Name = "DivideButton"
+        Me.DivideButton.Size = New System.Drawing.Size(60, 57)
+        Me.DivideButton.TabIndex = 10
+        Me.DivideButton.Text = "/"
+        Me.DivideButton.UseVisualStyleBackColor = True
+        '
+        'MultButton
+        '
+        Me.MultButton.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.MultButton.Location = New System.Drawing.Point(259, 276)
+        Me.MultButton.Name = "MultButton"
+        Me.MultButton.Size = New System.Drawing.Size(60, 57)
+        Me.MultButton.TabIndex = 11
+        Me.MultButton.Text = "*"
+        Me.MultButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.ClearButton.Location = New System.Drawing.Point(259, 87)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(60, 57)
+        Me.ClearButton.TabIndex = 9
+        Me.ClearButton.Text = "C"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'MinusButton
+        '
+        Me.MinusButton.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.MinusButton.Location = New System.Drawing.Point(259, 213)
+        Me.MinusButton.Name = "MinusButton"
+        Me.MinusButton.Size = New System.Drawing.Size(60, 57)
+        Me.MinusButton.TabIndex = 8
+        Me.MinusButton.Text = "-"
+        Me.MinusButton.UseVisualStyleBackColor = True
+        '
+        'DecimalPointButton
+        '
+        Me.DecimalPointButton.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.DecimalPointButton.Location = New System.Drawing.Point(126, 339)
+        Me.DecimalPointButton.Name = "DecimalPointButton"
+        Me.DecimalPointButton.Size = New System.Drawing.Size(60, 57)
+        Me.DecimalPointButton.TabIndex = 5
+        Me.DecimalPointButton.Text = "."
+        Me.DecimalPointButton.UseVisualStyleBackColor = True
+        '
+        'PlusButton
+        '
+        Me.PlusButton.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.PlusButton.Location = New System.Drawing.Point(259, 150)
+        Me.PlusButton.Name = "PlusButton"
+        Me.PlusButton.Size = New System.Drawing.Size(60, 57)
+        Me.PlusButton.TabIndex = 9
+        Me.PlusButton.Text = "+"
+        Me.PlusButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(392, 450)
+        Me.Controls.Add(Me.DivideButton)
+        Me.Controls.Add(Me.MultButton)
+        Me.Controls.Add(Me.PlusButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.MinusButton)
+        Me.Controls.Add(Me.DecimalPointButton)
+        Me.Controls.Add(Me.ZeroButton)
+        Me.Controls.Add(Me.EqualsButton)
+        Me.Controls.Add(Me.ThreeButton)
+        Me.Controls.Add(Me.TwoButton)
+        Me.Controls.Add(Me.OneButton)
+        Me.Controls.Add(Me.NineButton)
+        Me.Controls.Add(Me.EightButton)
+        Me.Controls.Add(Me.SevenButton)
+        Me.Controls.Add(Me.SixButton)
+        Me.Controls.Add(Me.FiveButton)
+        Me.Controls.Add(Me.FourButton)
+        Me.Controls.Add(Me.OutputTextBox)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Name = "Form1"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents OutputTextBox As TextBox
+    Friend WithEvents FourButton As Button
+    Friend WithEvents FiveButton As Button
+    Friend WithEvents SixButton As Button
+    Friend WithEvents NineButton As Button
+    Friend WithEvents EightButton As Button
+    Friend WithEvents SevenButton As Button
+    Friend WithEvents ThreeButton As Button
+    Friend WithEvents TwoButton As Button
+    Friend WithEvents OneButton As Button
+    Friend WithEvents EqualsButton As Button
+    Friend WithEvents ZeroButton As Button
+    Friend WithEvents DivideButton As Button
+    Friend WithEvents MultButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents MinusButton As Button
+    Friend WithEvents DecimalPointButton As Button
+    Friend WithEvents PlusButton As Button
+End Class
---
+++ Basic Calculator/Form1.vb
@@ -0,0 +1,78 @@
+﻿Public Class Form1
+
+    'The current value being input
+    Dim CurrentInput As String = ""
+
+    'The mathematical operator currently being used
+    Dim CurrentOperator As String
+
+    'The Actual Numeric value processed so far
+    Dim CurrentValue As Double = 0.0
+
+    Dim ValueOne As Double = 0.0
+    Dim ValueTwo As Double = 0.0
+
+
+#Region "General-Functions" 'General-purpose functions
+
+    ''' <summary>
+    ''' Add a digit to the number being processed currently
+    ''' </summary>
+    ''' <param name="Digit"></param>
+    Sub AddDigit(Digit As Double)
+
+        ValueTwo *= 10
+        ValueTwo += Digit
+
+    End Sub
+
+    ''' <summary>
+    ''' Set the current Mathematical operator
+    ''' </summary>
+    ''' <param name="Op"></param>
+    Sub SetOperator(Op As String)
+        CurrentOperator = Op
+    End Sub
+
+    Sub DigitClicked(DigitButton As Button)
+        AddDigit(DigitButton.Text)
+    End Sub
+
+
+#End Region
+
+
+
+#Region "Event-Handlers"
+    Private Sub OutputTextBox_TextChanged(sender As Object, e As EventArgs) Handles OutputTextBox.TextChanged
+
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+
+    End Sub
+
+    ''' <summary>
+    ''' Handle digit button clicks
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub DigitButton_Click(sender As Button, e As EventArgs) Handles DecimalPointButton.Click, ZeroButton.Click, OneButton.Click, TwoButton.Click, ThreeButton.Click, FourButton.Click, FiveButton.Click, SixButton.Click, SevenButton.Click, EightButton.Click, NineButton.Click
+        MsgBox(sender.Text)
+    End Sub
+
+
+
+    Private Sub OperatorButton_Click(sender As Object, e As EventArgs) Handles PlusButton.Click, MinusButton.Click, MultButton.Click, DivideButton.Click
+
+    End Sub
+
+
+    Private Sub EqualsButton_Click(sender As Object, e As EventArgs) Handles EqualsButton.Click
+        MsgBox("You work")
+    End Sub
+
+#End Region 'Button Event handlers
+
+
+End Class
